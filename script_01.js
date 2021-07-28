65aece55f76465dc81ad8932e6c83aeb9cbaaecc
CC-Nobody <cc.adiermann@gmail.com>
2021-07-28 11:36:12 +0200

    added Ausgabe + var firstName

--- script_01.js
+++ script_01.js
@@ -1,2 +1,19 @@
 /***** Variablen 01 *******/
 
+/*
+Dies ist ein richtiges Block-Kommentar
+*/
+
+// Das ist ein Zeilen-Kommentar
+
+//Variablen
+var firstName = "Jesus";
+
+// Ausgabe
+console.log("Hallo du öde Welt!");
+console.log("Wadde hade dudeda?");
+console.log("Hier könnte Ihre Werbung stehen.");
+console.log("Wilkommen zurück:",firstName);
+
+// Deklaration + Wertzuweisung I
+
