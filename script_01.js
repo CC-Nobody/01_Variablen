2ca54ef6ddd3748fb2224c4154858afc91eafae8
CC-Nobody <cc.adiermann@gmail.com>
2021-07-28 13:09:37 +0200

    endstand 28.07.2021

--- script_01.js
+++ script_01.js
@@ -21,9 +21,23 @@ console.log(lastName + ", " + firstName,lastName); // Ausgabe der Variablen
 */
 
 // 03 Deklaration + Wertzuweisung II
-
+/*
 let fullName,currentResidence, svn;
-fullName = prompt("Wer bist du?")
-currentResidence = prompt("Wo kommst du her?")
-svn = prompt("Wie lautet Ihre Sozialversicherungsnummer?")
+fullName = prompt("Wer bist du?");
+currentResidence = prompt("Wo kommst du her?");
+svn = prompt("Wie lautet Ihre Sozialversicherungsnummer?");
 console.log("Hallo ich bin",fullName,"ich komme aus",currentResidence + "."); // Ausgabe
+*/
+
+// JavaScript ist eine untypisierte Sprache! | untyped
+
+let test;
+test = "Value X"; //String
+test = 2; //Number
+test = false; //Boolean
+
+// test = prompt("Ich bitte um eine Eingabe:"); //String
+// string, number & boolean are primitive variables
+
+console.log("Datentyp:",typeof test);
+console.log("Inhalt:",test);
