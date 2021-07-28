d0e9f7be08fa71494dae87af9b3d928b68ff3508
CC-Nobody <cc.adiermann@gmail.com>
2021-07-28 12:33:57 +0200

    03 Deklaration + Wertzuweisung II

--- script_01.js
+++ script_01.js
@@ -6,16 +6,24 @@ Dies ist ein richtiges Block-Kommentar
 
 // Das ist ein Zeilen-Kommentar
 
-//Variablen
+
+/* Ausgabe
 var firstName = "Jesus"; // C Variante der Variablen definition
-
-// Ausgabe
 console.log("Welcome back:",firstName);
-
+*/
 
 
 // Deklaration + Wertzuweisung I
-
+/*
 let lastName; //Deklaration (Definition)
 lastName = "Christ"; // Wertzuweisung
 console.log(lastName + ", " + firstName,lastName); // Ausgabe der Variablen
+*/
+
+// 03 Deklaration + Wertzuweisung II
+
+let fullName,currentResidence, svn;
+fullName = prompt("Wer bist du?")
+currentResidence = prompt("Wo kommst du her?")
+svn = prompt("Wie lautet Ihre Sozialversicherungsnummer?")
+console.log("Hallo ich bin",fullName,"ich komme aus",currentResidence + "."); // Ausgabe
