b7f55253fd9ced177640c8714d1ed0de36544541
CC-Nobody <cc.adiermann@gmail.com>
2021-07-29 11:51:21 +0200

    Added Var.03

--- script_01.js
+++ script_01.js
@@ -31,6 +31,7 @@ console.log("Hallo ich bin",fullName,"ich komme aus",currentResidence + "."); //
 
 // JavaScript ist eine untypisierte Sprache! | untyped
 
+/*
 let test;
 test = "Value X"; //String
 test = 2; //Number
@@ -41,3 +42,33 @@ test = false; //Boolean
 
 console.log("Datentyp:",typeof test);
 console.log("Inhalt:",test);
+*/
+
+/******************* Variaben 02 *********************/
+/*
+// Deklaration
+let ageJohn, ageMark;
+let birthYearJohn, birthYearMark;
+
+//Wertzuweisung | Assignment
+let year = new Date();
+ageJohn = 25;
+ageMark = 30;
+
+// Operation
+birthYearJohn = year.getFullYear() - ageJohn;
+birthYearMark = year.getFullYear() - ageMark;
+
+console.log("birthYearJohn:",birthYearJohn);
+console.log("birthYearMark:", birthYearMark);
+console.log("date:", year.getFullYear());
+*/
+
+/*********  Variablen 03 ********/
+
+const ageJohn = parseInt(prompt("How old is John?"));
+const ageMark = 30;
+
+// Operation (Vergleich/Test)
+let isJohnOlder = (ageJohn > ageMark);
+console.log("Is John older?", isJohnOlder);
